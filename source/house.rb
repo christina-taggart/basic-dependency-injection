cf0a2bab19c6aab0841e8e1dd6218851633a62c4
mh120888 <matthew.alan.higgins@gmail.com>
2014-01-15 15:56:44 -0800

    adding release one, i have implemented fetch, no problems with nil

--- source/house.rb
+++ source/house.rb
@@ -1,36 +1,20 @@
 class House
-  attr_reader :square_feet, :num_bedrooms, :num_baths, :cost
-
-  # def initialize(address, square_feet, num_bedrooms = 3, num_baths = 2, cost = 320_000, down_payment = 0.20, sold = false, has_tenants = false)
-  #   @address = address
-  #   @square_feet = square_feet
-  #   @num_bedrooms = num_bedrooms
-  #   @num_baths = num_baths
-  #   @cost = cost
-  #   @down_payment = down_payment
-  #   @sold = sold
-  #   @short_sale = short_sale
-  #   @has_tenants = has_tenants
-  # end
+  attr_reader :square_feet, :num_bedrooms, :num_baths, :cost, :has_tenants, :sold, :down_payment
 
   def initialize(information = {})
-    @address = information['address']
-    @square_feet = information['square_feet']
-    @num_bedrooms = information['num_bedrooms'] || 3
-    @num_baths = information['num_baths'] || 2
-    @cost = information['cost'] || 320_000
-    @down_payment = information['down_payment'] || 0.20
-    @sold = information['sold'] || false
-    @short_sale = information['short_sale'] || false
-    @has_tenants = information['has_tenants'] || false
-  end
-
-  def obscure_address
-    @address.sub(/\A\d*/, '****')
+    @address = information.fetch(:address)
+    @square_feet = information.fetch(:square_feet)
+    @num_bedrooms = information.fetch(:num_bedrooms, 3)
+    @num_baths = information.fetch(:num_baths, 2)
+    @cost = information.fetch(:cost, 320_000)
+    @down_payment = information.fetch(:down_payment, 0.20)
+    @sold = information.fetch(:sold, false)
+    @short_sale = information.fetch(:short_sale, false)
+    @has_tenants = information.fetch(:has_tenants, false)
   end
 
   def buy!(money, good_credit)
-    @sold = true if money >= down_payment && good_credit
+    @sold = true if qualified_buyer?(money, good_credit)
   end
 
   def down_payment
@@ -40,20 +24,39 @@ class House
   def to_s
     "#{obscure_address} : #{square_feet} sq. ft., #{num_bedrooms} bed, #{num_baths} bath. $#{cost}"
   end
+
+  private
+
+  def qualified_buyer?(money, good_credit)
+    (money >= down_payment) && good_credit
+  end
+
+  def obscure_address
+    @address.sub(/\A\d*/, '****')
+  end
+
 end
 
 
-my_house = House.new({'address' => '2023 Parker Street',
-                      'square_feet' => 1000,
-                      'num_bedrooms' => 4,
-                      'num_baths' => 2,
-                      'cost' => 600,
-                      'down_payment' => 600,
-                      'sold' => false,
-                      'short_sale' => false,
-                      'has_tenants' => true
-                      })
-
+my_house = House.new({address: '2023 Parker Street',
+  square_feet: 1000,
+  num_bedrooms: 4,
+  num_baths: 2,
+  cost: 50000,
+  down_payment: 0.15,
+  sold: false,
+  short_sale: false,
+  has_tenants: true
+  })
 
 p my_house
+p my_house.has_tenants == true
+p my_house.square_feet == 1000
+p my_house.num_bedrooms == 4
+p my_house.num_baths == 2
+
+my_house.buy!(20000, true)
+p my_house.sold == true
+
+
 
