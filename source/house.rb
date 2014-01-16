70f93651b263f10e29d7e0a75c80cacfc68e6abc
Emmanuel Kaunitz <null>
2014-01-15 17:34:52 -0800

    default value true, using fetch prevents falsey, values from return.

--- source/house.rb
+++ source/house.rb
@@ -1,16 +1,18 @@
 class House
   attr_reader :square_feet, :num_bedrooms, :num_baths, :cost
 
-  def initialize(address, square_feet, num_bedrooms = 3, num_baths = 2, cost = 320_000, down_payment = 0.20, sold = false, has_tenants = false)
-    @address = address
-    @square_feet = square_feet
-    @num_bedrooms = num_bedrooms
-    @num_baths = num_baths
-    @cost = cost
-    @down_payment = down_payment
-    @sold = sold
-    @short_sale = short_sale
-    @has_tenants = has_tenants
+  def initialize(house_attributes = {})
+    #, address, square_feet, num_bedrooms = 3, num_baths = 2, cost = 320_000, down_payment = 0.20, sold = false, has_tenants = false
+    @address = house_attributes[:address]
+    @square_feet = house_attributes[:square_feet]
+    @num_bedrooms = house_attributes.fetch(:num_bedrooms) {3}
+    @num_baths = house_attributes.fetch(:num_baths) {2}
+    @cost = house_attributes.fetch(:cost) {320_000}
+    @down_payment = house_attributes.fetch(:down_payment) {0.20}
+    @sold = house_attributes.fetch(:sold) {false}
+    @short_sale = house_attributes[:short_sale]
+    @has_tenants = house_attributes.fetch(:has_tenants) {false}
+
   end
 
   def obscure_address
@@ -29,3 +31,6 @@ class House
     "#{obscure_address} : #{square_feet} sq. ft., #{num_bedrooms} bed, #{num_baths} bath. $#{cost}"
   end
 end
+
+house = House.new({:address =>"10315 rougemont ln", :square_feet => "2500"})
+p house
